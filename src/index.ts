d76ff2324c9aae6c9b6fac6b21e8a9b2c1efa3b2
june <juniper@aethre.co>
2024-11-10 21:20:47 -0500

    add a couple more exports

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,10 @@ export {
 	validateChoice,
 	type LeavingEarthGame,
 } from "./game";
+
+export type { Choice } from "./state/choice/Choice";
+export type { Decision } from "./state/decision/Decision";
+export type { Interrupt } from "./state/interrupt/Interrupt";
+export type { Model } from "./state/model/Model";
+
+export type { Draft, Immutable } from "laika-engine";
